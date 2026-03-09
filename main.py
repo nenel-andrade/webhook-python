e74c0e907e1e538ef53871d37a4143ff020455e9
nenel-andrade <manoscreu@gmail.com>
2026-03-09 13:46:23 -0300

    webhook base working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import HTMLResponse, PlainTextResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI(
     title="Webhook Contador",
@@ -7,3 +7,17 @@ app = FastAPI(
     version="0.1.0"
 )
 
+@app.post("/webhook", response_class=PlainTextResponse)
+async def webhook(request: Request):
+    body = await request.json()  
+    print(body)
+    # isso já é um dict Python arbitrário
+    # Agora body é exatamente o que veio no JSON, sem validação rígida
+    
+    # Sua lógica aqui:
+    # timestamp = ...
+    # texto_da_acao = extrair_acao_body(body)
+    # acao_encontrada = detectar_acao(texto_da_acao)
+    # etc...
+
+    return f"Retorno recebido com sucesso"
